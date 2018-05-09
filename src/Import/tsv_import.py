59330f49cd77bda79ae20b5ac8698f43c05323e2
Anthony Fejes <apfejes@users.noreply.github.com>
2018-05-08 21:22:16 -0700

    WIP on import.

--- src/Import/tsv_import.py
+++ src/Import/tsv_import.py
@@ -9,13 +9,34 @@ class Importer(object):
     def __init__(self, filename=None):
         self.filename = filename
         self.data = []
+        self.headers = None
 
+        self.read_file()
+        self.validate()
+
+    def read_file(self):
+        decode_required = False
         if self.filename.endswith("gz"):
             open_function = gzip.open
+            decode_required = True
         else:
             open_function = open
 
+        header_read = False
+
         with open_function(self.filename) as f:
             for line in f:
-                self.data.append(line)
+                if decode_required:
+                    line = line.decode('utf-8')
+                line = line.strip()
+                if not header_read:
+                    self.headers = line.split("\t")
+                    header_read = True
+                else:
+                    self.data.append(line.split("\t"))
 
+    def validate(self):
+        columns = len(self.data[1])
+        for row in self.data:
+            if len(row) != columns:
+                raise Exception("Inconsistent number of rows")
--- tests/test_template.py
+++ tests/test_template.py
@@ -9,9 +9,9 @@ class TestImport(unittest.TestCase):
     def test_import1(self):
         filename = "../data/GSE49712_HTSeq.txt.gz"
         import_module = Importer(filename=filename)
-        length = len(import_module.data)
 
-        self.assertEqual(21717, length)
+        self.assertEqual(10, len(import_module.headers))
+        self.assertEqual(21716, len(import_module.data))
 
 
 
