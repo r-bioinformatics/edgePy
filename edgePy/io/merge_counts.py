9f9b6ac0516a54f5f8d5785f0e7ce3f779a1a1d1
sridhar0605 <sridhar0605@gmail.com>
2018-05-11 11:48:56 -0500

    script to merge files from htseq counts removed args options

--- edgePy/io/merge_counts.py
+++ edgePy/io/merge_counts.py
@@ -12,11 +12,11 @@ def merge(dir_path):
 	merged_count files in the same directory
 
 	"""
-	files_to_merge = [f for f in os.listdir(args.d) if f.endswith('.tsv')]
-	_df = pd.read_csv(args.d+files_to_merge[0], sep='\t',names=['id','count'], index_col=0)
+	files_to_merge = [f for f in os.listdir(dir_path) if f.endswith('.tsv')]
+	_df = pd.read_csv(dir_path+files_to_merge[0], sep='\t',names=['id','count'], index_col=0)
 	final_df = pd.DataFrame(data=None, index=_df.index)
 	for f in files_to_merge: 
-		df_to_add = pd.read_csv(args.d+f, sep='\t', index_col=0, comment='_', names=[f.split('.')[0].split('/')[-1]]) 
+		df_to_add = pd.read_csv(dir_path+f, sep='\t', index_col=0, comment='_', names=[f.split('.')[0].split('/')[-1]]) 
 		final_df = pd.merge(final_df, df_to_add, left_index=True, right_index=True) 
 
 	final_df.to_csv(dir_path+'htseq_counts_output_merged.tsv', sep='\t')
