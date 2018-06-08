7b4e8a82c2b11d5761da275e0d18633e21d69bac
Clint Valentine <valentine.clint@gmail.com>
2018-06-07 19:09:21 -0700

    DGEList class with simple attr validation and data reading from open handles. (#14)
    * Draft DGEList with ability to read io handles
    
    * added cpm and logcpm normalization functions
    
    * Deleted VS Code settings files

--- edgePy/_DGEList.py
+++ edgePy/_DGEList.py
@@ -1,3 +1,5 @@
+import re
+
 import numpy as np
 
 __all__ = [
@@ -6,41 +8,182 @@ __all__ = [
 
 
 class DGEList(object):
+    """Class containing read counts over genes for multiple samples and their
+    corresponding metadata.
+
+    Parameters
+    ----------
+    counts : np.matrix, optional
+        A two-dimensional array with the dtype of `int`. Columns correspond to
+        samples and row to genes.
+    samples : np.array, optional
+        Array of sample names., same length as ncol(counts).
+    genes : np.array, optional
+        Array of gene names, same length as nrow(counts).
+    norm_factors : np.array, optional
+        Weighting factors for each sample, same length as ncol(counts).
+    group : np.array, optional
+        ...
+    remove_zeroes : bool
+        Whether to remove genes with zero counts for all samples.
+
+    Examples
+    --------
+    >>> import gzip
+    >>> from edgePy.io import get_dataset_path
+    >>> dataset = 'GSE49712_HTSeq.txt.gz'
+    >>> DGEList.read_handle(gzip.open(get_dataset_path(dataset)))
+    DGEList(num_samples=10, num_genes=21,717)
+
+    """
+
+    # Pattern to delete from field names anytime they are assigned.
+    _field_strip_re = re.compile(r'[\s"]+')
 
     def __init__(
         self,
         counts=None,
-        norm_factors=None,
         samples=None,
-        group=None,
         genes=None,
-        remove_zeroes=False
+        norm_factors=None,
+        group=None,
+        to_remove_zeroes=True
     ):
-        self.counts = counts if counts is not None else np.zeros(3)
+        if counts is None:
+            counts = np.matrix(np.zeros(3))
+        if norm_factors is None:
+            norm_factors = np.ones(np.size(counts, 1))
 
-        # Library size is an array of the sums of each column of counts, same as edgeR.
-        self.lib_size = np.sum(counts, 0)
+        self.to_remove_zeroes = to_remove_zeroes
+        self.counts = counts
+        self.samples = samples
+        self.genes = genes
+        self.norm_factors = norm_factors
+        self.group = group
 
-        # `norm_factors` multiplies each column by one unless specified (I think?).
-        # Same behavior as edgeR.
-        if norm_factors is not None:
-            self.norm_factors = norm_factors
-        else:
-            self.norm_factors = np.ones(np.shape(counts)[0])
+    @staticmethod
+    def _format_fields(fields):
+        for field in fields:
+            try:
+                field = field.decode()
+            except AttributeError:
+                pass
+            yield DGEList._field_strip_re.sub('', field)
 
     @property
     def counts(self):
+        """The read counts for the genes in all samples.
+
+        Returns
+        -------
+        counts : np.matrix
+
+        """
         return self._counts
 
     @counts.setter
     def counts(self, counts):
-        # Removes rows where counts are all zero.
-        counts = counts[np.all(counts != 0, axis=1)]
+        """Validate setting ``DGEList.counts`` for the illegal conditions:
 
-        # Basic sanity checks on the counts matrix before assigning it.
-        if np.any(counts < 0):
-            raise ValueError("Counts matrix cannot contain negative values")
+            * Negative values
+            * Values that are not numbers
+
+        Parameters
+        ----------
+        counts : np.matrix
+
+        """
+        if not isinstance(counts, np.ndarray):
+            raise ValueError('Counts matrix must be of type ``np.ndarray``.')
+        elif np.any(counts < 0):
+            raise ValueError('Counts matrix cannot contain negative values.')
         elif np.any(counts == np.nan):
-            raise ValueError("Counts must have real values")
+            raise ValueError('Counts matrix must have only real values.')
+        if self.to_remove_zeroes:
+            counts = counts[np.all(counts != 0, axis=1)]
 
-        self._counts = counts
+        self._counts = np.matrix(counts)
+
+    @property
+    def samples(self):
+        """Array of sample names."""
+        return self._samples
+
+    @samples.setter
+    def samples(self, samples):
+        # TODO: Validate samples here
+        # - Samples same length as ncol(self.counts) if defined
+        samples = np.array(list(DGEList._format_fields(samples)))
+        self._samples = samples
+
+    @property
+    def genes(self):
+        """Array of gene names."""
+        return self._genes
+
+    @genes.setter
+    def genes(self, genes):
+        # TODO: Validate genes here
+        # - Genes same length as nrow(self.counts) if defined
+        genes = np.array(list(DGEList._format_fields(genes)))
+        self._genes = genes
+
+    @property
+    def library_size(self):
+        """The total read counts per sample.
+
+        Returns
+        -------
+        library_size : np.matrix
+
+        """
+        return np.sum(self.counts, 0)
+
+    @classmethod
+    def read_handle(cls, handle, **kwargs):
+        """Read in a file-like object of delimited data for instantiation.
+
+        Parameters
+        ----------
+        handle : file-like object
+            Any handle supporting text streaming io.
+        kwargs : mappable
+            Additional arguments supported by ``np.genfromtxt``.
+
+        Returns
+        -------
+        DGEList
+            Container for storing read counts for samples from target genes.
+
+        """
+        # First column is the header for the the gene names.
+        # Remaining columns are sample names.
+        _, *samples = next(handle).strip().split()
+
+        genes = []
+        frame = np.genfromtxt(
+            fname=handle,
+            dtype=np.int,
+            converters={0: lambda _: genes.append(_) or 0},
+            autostrip=kwargs.pop('autostrip', True),
+            replace_space=kwargs.pop('replace_space', '_'),
+            case_sensitive=kwargs.pop('case_sensitive', True),
+            invalid_raise=kwargs.pop('invalid_raise', True),
+            **kwargs
+        )
+
+        # Delete the first column as it is copied on assignment to `genes`.
+        counts = np.delete(frame, 0, axis=1)
+
+        return DGEList(counts=counts, samples=samples, genes=genes)
+
+    def __repr__(self):
+        """Give a pretty non-executeable representation of this object."""
+        num_samples = len(self.samples) if self.samples is not None else 0
+        num_genes = len(self.genes) if self.genes is not None else 0
+
+        return (
+            f'{self.__class__.__name__}('
+            f'num_samples={num_samples:,}, '
+            f'num_genes={num_genes:,})'
+        )
