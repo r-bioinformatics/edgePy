2eb87ebad9e29b1186b7a2f2aa02300ba7f3d0c6
Anthony Fejes <apfejes@users.noreply.github.com>
2018-08-07 12:50:26 -0700

    remove redundant parameter.

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
 from setuptools import setup
 
 # Name and version specified here for `sdist` only.
-setup(name='edgePy', version='0.0.1', include_package_data=True)
+setup(name='edgePy', version='0.0.1')
