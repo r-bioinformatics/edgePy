f4dc3aa959b1297ca3e8d6bf636b97527029a7c4
Carbyne <carbynegit@gmail.com>
2018-08-11 00:06:49 +0200

    Change zipfile decompression to use smart_open library

--- edgePy/io/_io.py
+++ edgePy/io/_io.py
@@ -1,23 +1,16 @@
 """ Skeleton class for importing files """
 
-import gzip
+from smart_open import smart_open  # type: ignore
 
 from pathlib import Path
-from typing import Any, List, Tuple, Union
+from typing import List, Union
 
 
 __all__ = [
     'GroupImporter',
     'DataImporter',
     'get_dataset_path',
-    'get_open_function'
 ]
-
-
-def get_open_function(filename: str) -> Tuple[Any, bool]:
-    decode_required = filename.endswith("gz")
-    open_function = gzip.open if decode_required else open
-    return open_function, decode_required
 
 
 class GroupImporter(object):
@@ -29,13 +22,8 @@ class GroupImporter(object):
         self.read_file()
 
     def read_file(self) -> None:
-        open_function, decode_required = get_open_function(filename=self.filename)
-        with open_function(self.filename) as f:
+        with smart_open(self.filename, 'r') as f:
             for line in f:
-                if decode_required:
-                    # this is needed if we are using gzip, which returns a
-                    # binary-string.
-                    line = line.decode('utf-8')
                 line = line.strip().split(":")
                 if len(line) < 2:
                     continue  # blank line, or does not have any samples.
@@ -60,16 +48,10 @@ class DataImporter(object):
         self.validate()
 
     def read_file(self) -> None:
-        open_function, decode_required = get_open_function(filename=self.filename)
-
         header_read = False
 
-        with open_function(self.filename) as f:
+        with smart_open(self.filename, 'r') as f:
             for line in f:
-                if decode_required:
-                    # this is needed if we are using gzip, which returns a
-                    # binary-string.
-                    line = line.decode('utf-8')
                 line = line.strip().split("\t")
                 if not header_read:
                     _, *self.samples = line
