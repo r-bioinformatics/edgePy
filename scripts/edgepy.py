60ff2b14be1b731d3b3ec12aa3fcd2ab74b689a9
Anthony Fejes <apfejes@users.noreply.github.com>
2018-08-16 11:11:17 -0700

    functioning, and slightly cleaner edgepy script.   Remove Statistics module with KS-test, in favour of Scipy.

--- scripts/edgepy.py
+++ scripts/edgepy.py
@@ -11,6 +11,8 @@ import numpy as np
 from scipy.stats import ks_2samp
 from smart_open import smart_open
 
+from typing import List, Dict, Hashable, Any
+
 import configparser
 
 
@@ -31,6 +33,10 @@ def parse_arguments(parser=None):
     parser.add_argument("--mongo_key_value", default="RNA-Seq1")
 
     parser.add_argument("--output", help="optional output file for results")
+    parser.add_argument("--cutoff", help="p-value cutoff to accept.", default=0.05)
+    parser.add_argument(
+        "--minimum_cpm", help="discard results for which no group has this many counts", default=10
+    )
 
     args = parser.parse_args()
 
@@ -64,9 +70,9 @@ class EdgePy(object):
 
             self.dge_list.write_npz_file("./edgePy/data/example_data.cpe")
 
-            self.ensg_to_symbol = mongo_importer.mongo_reader.find_as_dict('ensembl_90_37', "symbol_by_ensg", query={})
-            # for r in self.ensg_to_symbol:
-            #     print(r, self.ensg_to_symbol[r])
+            self.ensg_to_symbol = mongo_importer.mongo_reader.find_as_dict(
+                'ensembl_90_37', "symbol_by_ensg", query={}
+            )
 
         else:
             importer = DataImporter(args.count_file)
@@ -78,7 +84,8 @@ class EdgePy(object):
             # self.dge_list = ???
 
         self.output = args.output if args.output else None
-
+        self.p_value_cutoff = args.cutoff
+        self.minimum_cpm = args.minimum_cpm
 
     @staticmethod
     def group_type(g):
@@ -93,14 +100,13 @@ class EdgePy(object):
 
     def run_ks(self):
 
-        gene_likelyhood1 = {}
-        # self.dge_list.group = ['A', 'A', 'A', 'A', 'A', 'B', 'B', 'B', 'B', 'B']
+        gene_likelyhood1: Dict[Hashable, float] = {}
 
         print(self.dge_list.group)
         group_types = set(self.dge_list.group)
         group_types = list(group_types)
-        group_filters = {}
-        gene_details = {}
+        group_filters: Dict[Hashable, Any] = {}
+        gene_details: Dict[Hashable, Dict[Hashable, Any]] = {}
         for group in group_types:
             group_filters[group] = [g == group for g in self.dge_list.group]
         for gene_idx, gene in enumerate(self.dge_list.genes):
@@ -113,34 +119,55 @@ class EdgePy(object):
                 mean2 = np.mean(group_data2)
                 gene_likelyhood1[gene] = ks_2samp(group_data1, group_data2)[1]
 
-                gene_details[gene] = {'mean1': mean1,
-                                      'mean2': mean2}
+                gene_details[gene] = {'mean1': mean1, 'mean2': mean2}
 
-        sorted_likely = [(gene, gene_likelyhood1[gene]) for gene in sorted(gene_likelyhood1, key=gene_likelyhood1.get)]
-
-        print(f"gene_name p-value {group_types[0]} {group_types[1]}")
+        results = self.generate_results(
+            gene_details, gene_likelyhood1, group_types[0], group_types[1]
+        )
 
         if self.output:
             with smart_open(self.output, 'w') as out:
-                out.write(f"gene_name p-value {group_types[0]} {group_types[1]}\n")
-                for gene, p in sorted_likely:
-                    m1 = gene_details[gene]['mean1']
-                    m2 = gene_details[gene]['mean2']
-
-                    if p < 0.05 and (m1 > 10 or m2 > 10) and m1 < m2:
-                        out.write(
-                            (f"{self.ensg_to_symbol[gene]['symbols'][0] if gene in self.ensg_to_symbol else gene} "
-                             f"{gene_likelyhood1[gene]} "
-                             f"{gene_details[gene]['mean1']:.2f} "
-                             f"{gene_details[gene]['mean2']:.2f}\n"))
+                out.writelines(results)
             print(f"wrote to {self.output}")
+        else:
+            for line in results:
+                print(line)
 
-        # for gene, p in sorted_likely:
-        #     if p < 0.05 and (gene_details[gene]['mean1'] > 10 or gene_details[gene]['mean2'] > 10):
-        #         print(f"{self.ensg_to_symbol[gene]['symbols'][0] if gene in self.ensg_to_symbol else gene} "
-        #               f"{gene_likelyhood1[gene]} "
-        #               f"{gene_details[gene]['mean1']:.2f} "
-        #               f"{gene_details[gene]['mean2']:.2f}")
+    def generate_results(
+        self,
+        gene_details: Dict[Hashable, Dict[Hashable, Any]],
+        gene_likelyhood1: Dict[Hashable, float],
+        group_type1: str,
+        group_type2: str,
+    ) -> List[str]:
+
+        results: List[str] = []
+        sorted_likely = [
+            (gene, gene_likelyhood1[gene])
+            for gene in sorted(gene_likelyhood1, key=gene_likelyhood1.get)
+        ]
+        results.append(f"gene_name p-value {group_type1} {group_type2}")
+        for gene, p in sorted_likely:
+            m1 = gene_details[gene]['mean1']
+            m2 = gene_details[gene]['mean2']
+            symbol = (
+                self.ensg_to_symbol[gene]['symbols'][0] if gene in self.ensg_to_symbol else gene
+            )
+
+            if (
+                p < self.p_value_cutoff
+                and not (m1 < self.minimum_cpm and m2 < self.minimum_cpm)
+                and m1 < m2
+            ):
+                results.append(
+                    f"{gene} "
+                    f"{symbol} "
+                    f"{gene_likelyhood1[gene]} "
+                    f"{gene_details[gene]['mean1']:.2f} "
+                    f"{gene_details[gene]['mean2']:.2f}\n"
+                )
+
+        return results
 
 
 def main():
