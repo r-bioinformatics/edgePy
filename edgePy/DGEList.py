ee8f01c0a1cfebf0c05fb96aa39ab0533115768c
Anthony Fejes <apfejes@users.noreply.github.com>
2018-08-24 15:45:29 -0700

    Again, cpm does not return a value, currently.

--- edgePy/DGEList.py
+++ edgePy/DGEList.py
@@ -245,7 +245,7 @@ class DGEList(object):
         """
         return np.sum(self.counts, 0)
 
-    def cpm(self, log: bool = False, prior_count: float = PRIOR_COUNT) -> 'DGEList':
+    def cpm(self, log: bool = False, prior_count: float = PRIOR_COUNT) -> None:
         """Return the DGEList normalized to read counts per million."""
         self.counts = 1e6 * self.counts / np.sum(self.counts, axis=0)
         if log:
