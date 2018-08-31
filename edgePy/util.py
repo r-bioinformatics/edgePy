474b97c968e0963c416a6d100c68e7cd0d2537c7
S. Hutchins <ms.s.hutchins@gmail.com>
2018-08-31 18:36:02 -0500

    Added a util.py that includes the private function to setup the log.

---
+++ edgePy/util.py
@@ -0,0 +1,22 @@
+""" Utilities to support functions and classes """
+
+from logzero import setup_logger, LogFormatter, logging
+
+
+def _setup_log():
+    """Formats and sets up the logger instance.
+
+    Returns:
+        An instance of a logger.
+    """
+    _log_format = ("%(color)s[%(levelname)s] [%(asctime)s | "
+                   "%(module)s - line %(lineno)d]:%(end_color)s %(message)s")
+    _formatter = LogFormatter(fmt=_log_format)
+
+    logger = setup_logger(logfile=None, level=logging.INFO,
+                          formatter=_formatter)
+
+    return logger
+
+
+log = _setup_log()
