1252a7d40a8878a3952c8ce34af2988f966f6b2a
Anthony Fejes <apfejes@users.noreply.github.com>
2018-09-06 13:49:56 -0700

    functions required for rpkm calculation.

--- edgePy/data_import/ensembl/ensembl.py
+++ edgePy/data_import/ensembl/ensembl.py
@@ -14,13 +14,22 @@ class EnsemblResrouce:
         self.coding_genes: Optional[List] = None
 
     def get_gene_ids(self) -> List:
-        if not self.genes:
+        if self.genes is None:
             self.genes = [self.ensembl.gene_by_id(gene_id) for gene_id in self.gene_ids]
         return self.genes
 
     def get_coding_genes(self) -> List:
-        if not self.coding_genes:
+        if self.coding_genes is None:
             self.coding_genes = [
                 gene for gene in self.get_gene_ids() if gene.biotype == 'protein_coding'
             ]
         return self.coding_genes
+
+    def get_transcript_ids(self, gene_id: str):
+        raise NotImplementedError
+
+    def get_canonical_transcript_id(self, gene_id: str):
+        raise NotImplementedError
+
+    def get_length_of_transcript(self, gene_id: str):
+        raise NotImplementedError
--- tests/ensembl/test_ensembl.py
+++ tests/ensembl/test_ensembl.py
@@ -9,12 +9,19 @@ def ensembl():
 
 
 def test_get_gene_ids():
-    ens = ensembl()
-    gene_ids = ens.get_gene_ids()
+    gene_ids = ensembl().get_gene_ids()
     assert len(gene_ids) == 58395
 
 
 def test_coding_genes():
-    ens = ensembl()
-    coding_genes = ens.get_coding_genes()
+    coding_genes = ensembl().get_coding_genes()
     assert len(coding_genes) == 19912
+
+
+def test_non_implemented():
+    with pytest.raises(NotImplementedError):
+        ensembl().get_transcript_ids("gene")
+    with pytest.raises(NotImplementedError):
+        ensembl().get_canonical_transcript_id("gene")
+    with pytest.raises(NotImplementedError):
+        ensembl().get_length_of_transcript("gene")
